da0bcd6c9d8751941a261e940d2186a874d2b2b9
Alessandro Mandelli <alessandro.mandelli@costergroup.eu>
2018-11-12 11:35:48 +0100

    Aggiungere i file di progetto.

---
+++ MultiPanelSemaphor/MultiPanelSemaphor.vb
@@ -0,0 +1,151 @@
+﻿Public Class MultiPanelSemaphor
+
+    Const Pi As Double = Math.PI
+    Dim x1, y1, x2, y2, x3, y3 As Integer
+    Dim xp1, yp1, xp2, yp2 As Int32
+    Private m_semaphortext As String = "Text"
+    Private m_selectedPosition As Integer = 0
+    Private m_positions As Integer = 3
+    Private m_WarningThreshold As Integer = 80
+    Private m_semaphorColor As Color = Color.Black
+    Private m_isSemaphorBlinking As Boolean
+    Dim centreX As Double = 32
+    Dim centreY As Double = 44
+    Dim apen As New Pen(Color.LightGray, 1)
+    Dim lpen As New Pen(Color.Black, 2)
+    Dim angle As Double = -3 / 4 * Pi
+    Dim blinkingSemaphorTimer As New Timer
+    Dim pbSemaphor As New PictureBox
+    Dim lblSemaphorText As New Label
+    Public Property isSemaphorBlinking As Boolean
+        Get
+            isSemaphorBlinking = m_isSemaphorBlinking
+        End Get
+        Set(isSemaphorBlinking As Boolean)
+            m_isSemaphorBlinking = isSemaphorBlinking
+            Me.Refresh()
+        End Set
+    End Property
+    Public Property semaphorColor As Color
+        Get
+            semaphorColor = m_semaphorColor
+        End Get
+        Set(semaphorColor As Color)
+            Select Case semaphorColor
+                Case Color.Yellow
+                    pbSemaphor.Image = My.Resources.led_amber_black
+                Case Color.Blue
+                    pbSemaphor.Image = My.Resources.led_blue_black
+                Case Color.Green
+                    pbSemaphor.Image = My.Resources.led_green_black
+                Case Color.Red
+                    pbSemaphor.Image = My.Resources.led_red_black
+                Case Color.Black
+                    pbSemaphor.Image = My.Resources.led_off_black
+                Case Else
+                    pbSemaphor.Image = My.Resources.led_off_black
+                    Exit Property
+            End Select
+            m_semaphorColor = semaphorColor
+
+        End Set
+    End Property
+    Public Property semaphortext As String
+        Get
+            semaphortext = m_semaphortext
+        End Get
+        Set(semaphortext As String)
+            If semaphortext.Length < 10 Then
+                m_semaphortext = semaphortext
+                lblSemaphorText.Text = semaphortext
+            End If
+            Me.Refresh()
+        End Set
+    End Property
+    Sub New()
+
+        ' La chiamata è richiesta dalla finestra di progettazione.
+        InitializeComponent()
+
+        'AddHandler pbSwitch.Click, AddressOf Me_click
+
+        ' Aggiungere le eventuali istruzioni di inizializzazione dopo la chiamata a InitializeComponent().
+        AddHandler blinkingSemaphorTimer.Tick, AddressOf blinkingSemaphor
+        With pbSemaphor
+            .Location = New Point(6, 42)
+            .Size = New Size(52, 52)
+            .Name = "pbSemaphor"
+            .Visible = True
+            .Image = My.Resources.led_off_black
+            .SizeMode = PictureBoxSizeMode.Zoom
+        End With
+        Me.Controls.Add(pbSemaphor)
+
+        With lblSemaphorText
+            .Size = New Size(58, 18)
+            .Font = New Font("SegoeUI", 8.25, FontStyle.Regular)
+            .Name = "lblSemaphorText"
+            .Location = New Point(3, 3)
+            .Text = m_semaphortext
+            .Visible = True
+            .TextAlign = ContentAlignment.MiddleCenter
+        End With
+        Me.Controls.Add(lblSemaphorText)
+
+    End Sub
+    Private Sub GaugePaint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+
+
+        e.Graphics.DrawRectangle(apen, 1, 1, 62, 21)
+
+
+        If Not isSemaphorBlinking Then
+            With blinkingSemaphorTimer
+                .Enabled = False
+                .Stop()
+            End With
+            Select Case m_semaphorColor
+                Case Color.Yellow
+                    pbSemaphor.Image = My.Resources.led_amber_black
+                Case Color.Blue
+                    pbSemaphor.Image = My.Resources.led_blue_black
+                Case Color.Green
+                    pbSemaphor.Image = My.Resources.led_green_black
+                Case Color.Red
+                    pbSemaphor.Image = My.Resources.led_red_black
+            End Select
+
+        ElseIf isSemaphorBlinking Then
+            If blinkingSemaphorTimer.Enabled = False Then
+                With blinkingSemaphorTimer
+                    .Interval = 1000
+                    .Enabled = True
+                    .Start()
+                End With
+            End If
+        End If
+
+    End Sub
+    Private Sub blinkingSemaphor()
+
+        If m_semaphorColor <> Color.Black Then
+            If DateTime.Now.Second Mod 2 = 0 Then
+                Select Case m_semaphorColor
+                    Case Color.Yellow
+                        pbSemaphor.Image = My.Resources.led_amber_black
+                    Case Color.Blue
+                        pbSemaphor.Image = My.Resources.led_blue_black
+                    Case Color.Green
+                        pbSemaphor.Image = My.Resources.led_green_black
+                    Case Color.Red
+                        pbSemaphor.Image = My.Resources.led_red_black
+                End Select
+            Else
+                pbSemaphor.Image = My.Resources.led_off_black
+            End If
+        End If
+
+    End Sub
+End Class
+
+
